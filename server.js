aaadffed030ee9fa3a4081cb43424a0f5201f136
Brucimus <Brucimus@gmail.com>
2018-10-06 14:11:32 -0700

    db once again

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect(MONGODB_URI || "mongodb://localhost/scraperHW", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/scraperHW", { useNewUrlParser: true });
 // mongoose.connect(MONGODB_URI);
 
 // Handlebars

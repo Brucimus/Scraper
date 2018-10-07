1a9eead407ad66e04e51ac4e6c6d6d77d0f1e9e3
Brucimus <Brucimus@gmail.com>
2018-10-07 15:49:20 -0700

    adjusted modals to submit data to db

--- controllers/apiRoutes.js
+++ controllers/apiRoutes.js
@@ -65,13 +65,13 @@ module.exports = function (app) {
     
     // Route for grabbing a specific Product by id, populate it with it's note
     app.get("/products/:id", function(req, res) {
-        console.log("one");
+        // console.log("one");
         // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
         db.Product.findOne({ _id: req.params.id })
         // ..and populate all of the notes associated with it
         .populate("notes")
         .then(function(dbProduct) {
-            console.log("two");
+            // console.log("two");
             // If we were able to successfully find an Product with the given id, send it back to the client
             res.json(dbProduct);
         })
@@ -84,9 +84,13 @@ module.exports = function (app) {
     
     // Route for saving/updating an Product's associated Note
     app.post("/products/:id", function(req, res) {
+        console.log("one");
+
         // Create a new note and pass the req.body to the entry
         db.Note.create(req.body)
         .then(function(dbNote) {
+            console.log(req.params.id)
+            console.log(dbNote);
             // If a Note was created successfully, find one Product with an `_id` equal to `req.params.id` and push the new Note's _id to the Product's `notes` array
             // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
             // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -35,26 +35,20 @@ $.getJSON("/products", function(data) {
       .then(function(data) {
         // console.log(data);
         // The title of the article
-        console.log(data.notes.length);
-        for (var i=0; i < data.notes.length; i++) {
-          console.log(data.notes[i]);
-          $("#notes").append("<h5>" + data.notes[i] + "</h5>");
-          $("#notes").append("<p>" + data.notes[i] + "</p>");
-        }
-        // // An input to enter a new title
-        // $("#notes").append("<input id='titleinput' name='title' >");
-        // // A textarea to add a new note body
-        // $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
-        // // A button to submit a new note, with the id of the article saved to it
-        // $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
-  
-        // If there's a note in the article
-        // if (data.note) {
-        //   // Place the title of the note in the title input
-        //   $("#titleinput").val(data.note.title);
-        //   // Place the body of the note in the body textarea
-        //   $("#bodyinput").val(data.note.body);
-        // }
+        $("#notes").append("<div class='modal-body'>");
+          {/* console.log(data.notes.length); */}
+          for (var i=0; i < data.notes.length; i++) {
+            $("#notes").append("<h5>" + data.notes[i] + "</h5>");
+            $("#notes").append("<p>" + data.notes[i] + "</p>");
+          }
+        $("#notes").append("</div>");
+        $("#notes").append("<div class='modal-footer'>");
+          $("#notes").append("<form action='/submit' method='post'>");
+            $("#notes").append("<input type='text' name='product' placeholder='product' id='titleinput'>");
+            $("#notes").append("<input type='text' name='note' placeholder='note' id='bodyinput'>");
+            $("#notes").append("<input type='submit' data-dismiss='modal' id='savenote' data-id='" + thisId +"'>");
+          $("#notes").append("</form>");
+        $("#notes").append("</div>");
       });
   });
   
